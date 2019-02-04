a34a2e5221a047e7f79def8c0ac26d714aeb04fe
KaJuan Jones <47114033+kajuanjones@users.noreply.github.com>
2019-02-04 12:08:07 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Add the methods here...
@@ -43,6 +45,10 @@ class Book
     public String printTitle(){
         System.out.println();
         return title;
+    }
+    
+    public int getPages(){
+        return pages;
     }
     
    
